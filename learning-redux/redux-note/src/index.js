5431a9f7545e5388fba606f95a1bcb55498be0b9
JiayiLi <jiayi_li10@163.com>
2017-12-17 22:41:59 +0800

    update

--- learning-redux/redux-note/src/index.js
+++ learning-redux/redux-note/src/index.js
@@ -7,11 +7,15 @@ import warning from './utils/warning'
 import __DO_NOT_USE__ActionTypes from './utils/actionTypes'
 
 /*
- * This is a dummy function to check if the function name has been altered by minification.
- * If the function has been minified and NODE_ENV !== 'production', warn the user.
- */
+* This is a dummy function to check if the function name has been altered by minification.
+* If the function has been minified and NODE_ENV !== 'production', warn the user.
+*/
+// 用来检测在非生产环境下代码是否被压缩，主要目的是担心你在开发环境用了 redux 的压缩版本，导致在你压缩项目时 redux 再次被压缩出现代码不
+// 如果当前不是 production 环境而代码却被压缩了，则发出警告
+// function 有 name 属性，还有 arguments 、length 等属性 ，参考 https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Function/name
 function isCrushed() {}
 
+// 主要是通过 函数名是否还是原名 来判断
 if (
   process.env.NODE_ENV !== 'production' &&
   typeof isCrushed.name === 'string' &&
@@ -26,6 +30,7 @@ if (
   )
 }
 
+// 暴露 API
 export {
   createStore,
   combineReducers,
